2f89ca1bfaeded1b42d53c7fd839e375f44dcbb2
Tanishq Singh <hello@tanishqsingh.com>
2025-09-21 12:41:55 +0530

    web improved, readme add

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { createRoot } from "react-dom/client";
 
 import Home from "@/pages/home";
